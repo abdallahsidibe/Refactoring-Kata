e1ed9f124342c9fc1364705c64e9c1d938dc8fa9
abdallah monastir <sidibeabdallah01@gmail.com>
2022-08-21 21:18:10 +0200

    first commit

---
+++ src/yatzy/Category.java
@@ -0,0 +1,35 @@
+package yatzy;
+
+import java.util.function.Function;
+
+/**
+ * Category: associate categories with score calculations
+ * */
+public enum Category {
+    Chance(YatzyScores::chance),
+    Yatzy(YatzyScores::yatzy),
+    Ones(YatzyScores::ones),
+    Twos(YatzyScores::twos),
+    Threes(YatzyScores::threes),
+    Fours(YatzyScores::fours),
+    Fives(YatzyScores::fives),
+    Sixes(YatzyScores::sixes),
+    Pair(YatzyScores::pair),
+    TwoPairs(YatzyScores::twoPairs),
+    ThreeOfAKind(YatzyScores::threeOfAKind),
+    FourOfAKind(YatzyScores::fourOfAKind),
+    SmallStraight(YatzyScores::smallStraight),
+    LargeStraight(YatzyScores::largeStraight),
+    FullHouse(YatzyScores::fullHouse);
+
+    private final Function<DiceRoll, Integer> scoreFunction;
+
+    Category(Function<DiceRoll, Integer> scoreFunction) {
+        this.scoreFunction = scoreFunction;
+    }
+
+    public int score(DiceRoll roll) {
+        return this.scoreFunction.apply(roll);
+    }
+}
+ 
